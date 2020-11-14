2f98c73d45d61b5836c14c2ab93699fe174408cc
Bhavik-20 <bbhatt2005@gmail.com>
2020-11-14 18:57:28 +0530

    edited :student_dateSelect.dart added font : PlayfairDisplay-VariableFont_wght.ttf

--- lib/pages/student_dateSelect.dart
+++ lib/pages/student_dateSelect.dart
@@ -7,6 +7,7 @@ import 'package:flutter_appointment_app/services/rounded_button.dart';
 import 'package:flutter_appointment_app/services/rounded_input_field.dart';
 import 'package:flutter_appointment_app/services/rounded_password_field.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 void main(){
   runApp(dateSelect());
@@ -19,6 +20,9 @@ class dateSelect extends StatefulWidget {
 
 class _dateSelectState extends State<dateSelect> {
   DateTime _dateTime;
+  var day;
+  var full;
+  List days_of_week=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -50,7 +54,7 @@ class _dateSelectState extends State<dateSelect> {
                         border: Border.all(
                           color: Colors.white,
                         ),
-                        borderRadius: BorderRadius.all(Radius.circular(20))
+                        borderRadius: BorderRadius.all(Radius.circular(10))
                     ),
                     width: size.width*0.8,
                     child: Column(
@@ -60,15 +64,16 @@ class _dateSelectState extends State<dateSelect> {
                             child: TextField(
                               style: TextStyle(
                                   fontSize: 20.0,
-                                  fontFamily: 'kanit',
+                                  fontFamily: 'playfair',
+                                  fontWeight: FontWeight.bold
                               ),
                               enabled: false,
                               autocorrect: false,
                               autofocus: false,
                               cursorColor: Colors.black,
                               decoration: new InputDecoration(
-                                labelText: 'Teacher Name',
-                                border: InputBorder.none,
+                               labelText: 'Teacher Name',
+                               border: InputBorder.none,
                               ),
                             ),
                           ),
@@ -81,7 +86,8 @@ class _dateSelectState extends State<dateSelect> {
                             child: TextField(
                               style: TextStyle(
                                   fontSize: 20.0,
-                                  fontFamily: 'kanit'
+                                  fontFamily: 'playfair',
+                                  fontWeight: FontWeight.bold
                               ),
                               enabled: false,
                               autocorrect: false,
@@ -102,7 +108,8 @@ class _dateSelectState extends State<dateSelect> {
                             child: TextField(
                               style: TextStyle(
                                   fontSize: 20.0,
-                                  fontFamily: 'kanit'
+                                  fontFamily: 'playfair',
+                                  fontWeight: FontWeight.bold
                               ),
                               enabled: false,
                               autocorrect: false,
@@ -123,7 +130,8 @@ class _dateSelectState extends State<dateSelect> {
                             child: TextField(
                               style: TextStyle(
                                   fontSize: 20.0,
-                                  fontFamily: 'kanit'
+                                  fontFamily: 'playfair',
+                                fontWeight: FontWeight.bold
                               ),
                               enabled: false,
                               autocorrect: false,
@@ -135,27 +143,49 @@ class _dateSelectState extends State<dateSelect> {
                               ),
                             ),
                           ),
-
                         ]
                     )
                 ),
               ),
               SizedBox(height: 20.0,),
-              Text(_dateTime==null?'Select Date':_dateTime.toString()),
-              RaisedButton(
-                child: Text('Pick a Date:'),
-                onPressed: () {
-                  showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now().add(Duration(days: 2)),
-                      firstDate:  DateTime.now().add(Duration(days: 2)),
-                      lastDate: DateTime.now().add(Duration(days: 7)),
-                  ).then((date) {
-                    setState(() {
-                      _dateTime = date;
-                    });
-                  });
-                })
+              Row(
+                children: [
+                  SizedBox(width: 50.0,),
+                  Row(
+                    children: [
+                      Icon(Icons.calendar_today),
+                      SizedBox(width: 10.0,),
+                      RaisedButton(
+                          child: Text('Pick a Date:'),
+                          onPressed: () {
+                            showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now().add(Duration(days: 1)),
+                              firstDate:  DateTime.now().add(Duration(days: 1)),
+                              lastDate: DateTime.now().add(Duration(days: 7)),
+                            ).then((date) {
+                              setState(() {
+                                day=days_of_week[date.weekday-1];
+                                var formatter = new DateFormat('dd-MM-yyyy');
+                                String formatted = formatter.format(date);
+                                full=day+" : "+formatted;
+                                print(full);
+                              });
+                            });
+                          }),
+                    ],
+                  ),
+                  SizedBox(width: 15.0,),
+                  Text(full==null?'Select Date':full.toString(),
+                    style: TextStyle(
+                        fontFamily: 'playfair',
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              ),
+
             ],
           ),
         )
