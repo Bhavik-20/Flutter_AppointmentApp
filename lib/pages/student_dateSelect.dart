d57bfb4423f6a80e81c3ceaaad5b5c75e26aa9e5
Bhavik-20 <bbhatt2005@gmail.com>
2020-11-14 15:29:46 +0530

    edited :student_dateSelect.dart

--- lib/pages/student_dateSelect.dart
+++ lib/pages/student_dateSelect.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_appointment_app/services/constants.dart';
@@ -11,129 +12,150 @@ void main(){
   runApp(dateSelect());
 }
 
-class dateSelect extends StatelessWidget {
+class dateSelect extends StatefulWidget {
+  @override
+  _dateSelectState createState() => _dateSelectState();
+}
+
+class _dateSelectState extends State<dateSelect> {
+  DateTime _dateTime;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.deepPurple[100],
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: (){},
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
+        backgroundColor: Colors.deepPurple[100],
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: (){},
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
           ),
+          backgroundColor: Colors.deepPurple[600],
+          title: Text('Select Date and Time'),
+          centerTitle: true,
+          actions: [
+            Icon(Icons.access_time_sharp)
+          ],
         ),
-        backgroundColor: Colors.deepPurple[600],
-        title: Text('Select Date and Time'),
-        centerTitle: true,
-        actions: [
-          Icon(Icons.access_time_sharp)
-        ],
-      ),
         body: SingleChildScrollView(
           child: Column(
             children: [
               SizedBox(height: 30.0,),
               Center(
                 child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(
+                    decoration: BoxDecoration(
                         color: Colors.white,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(20))
-                  ),
-                  width: size.width*0.8,
-                  child: Column(
-                  children: [
-                      Container(
-                      height:50.0,
-                      child: TextField(
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'kanit'
+                        border: Border.all(
+                          color: Colors.white,
                         ),
-                        enabled: false,
-                        autocorrect: false,
-                        autofocus: false,
-                        cursorColor: Colors.black,
-                        decoration: new InputDecoration(
-                          labelText: 'Teacher Name',
-                          border: InputBorder.none,
-                        ),
-                      ),
+                        borderRadius: BorderRadius.all(Radius.circular(20))
                     ),
-                    Divider(
-                      color: Colors.deepPurple[100],
-                      thickness: 1,
-                    ),
-                    Container(
-                      height:50.0,
-                      child: TextField(
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'kanit'
-                        ),
-                        enabled: false,
-                        autocorrect: false,
-                        autofocus: false,
-                        cursorColor: Colors.black,
-                        decoration: new InputDecoration(
-                          labelText: 'Name',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    Divider(
-                      color: Colors.deepPurple[100],
-                      thickness: 1,
-                    ),
-                    Container(
-                      height:50.0,
-                      child: TextField(
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'kanit'
-                        ),
-                        enabled: false,
-                        autocorrect: false,
-                        autofocus: false,
-                        cursorColor: Colors.black,
-                        decoration: new InputDecoration(
-                          labelText: 'Name',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    Divider(
-                      color: Colors.deepPurple[100],
-                      thickness: 1,
-                    ),
-                    Container(
-                      height:50.0,
-                      child: TextField(
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'kanit'
-                        ),
-                        enabled: false,
-                        autocorrect: false,
-                        autofocus: false,
-                        cursorColor: Colors.black,
-                        decoration: new InputDecoration(
-                          labelText: 'Name',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
+                    width: size.width*0.8,
+                    child: Column(
+                        children: [
+                          Container(
+                            height:50.0,
+                            child: TextField(
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontFamily: 'kanit',
+                              ),
+                              enabled: false,
+                              autocorrect: false,
+                              autofocus: false,
+                              cursorColor: Colors.black,
+                              decoration: new InputDecoration(
+                                labelText: 'Teacher Name',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.deepPurple[100],
+                            thickness: 1,
+                          ),
+                          Container(
+                            height:50.0,
+                            child: TextField(
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontFamily: 'kanit'
+                              ),
+                              enabled: false,
+                              autocorrect: false,
+                              autofocus: false,
+                              cursorColor: Colors.black,
+                              decoration: new InputDecoration(
+                                labelText: 'Teacher Initials',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.deepPurple[100],
+                            thickness: 1,
+                          ),
+                          Container(
+                            height:50.0,
+                            child: TextField(
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontFamily: 'kanit'
+                              ),
+                              enabled: false,
+                              autocorrect: false,
+                              autofocus: false,
+                              cursorColor: Colors.black,
+                              decoration: new InputDecoration(
+                                labelText: 'Room Number',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.deepPurple[100],
+                            thickness: 1,
+                          ),
+                          Container(
+                            height:50.0,
+                            child: TextField(
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontFamily: 'kanit'
+                              ),
+                              enabled: false,
+                              autocorrect: false,
+                              autofocus: false,
+                              cursorColor: Colors.black,
+                              decoration: new InputDecoration(
+                                labelText: 'Email',
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
 
-                ]
-                )
-
+                        ]
+                    )
                 ),
-              )
+              ),
+              SizedBox(height: 20.0,),
+              Text(_dateTime==null?'Select Date':_dateTime.toString()),
+              RaisedButton(
+                child: Text('Pick a Date:'),
+                onPressed: () {
+                  showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now().add(Duration(days: 2)),
+                      firstDate:  DateTime.now().add(Duration(days: 2)),
+                      lastDate: DateTime.now().add(Duration(days: 7)),
+                  ).then((date) {
+                    setState(() {
+                      _dateTime = date;
+                    });
+                  });
+                })
             ],
           ),
         )
