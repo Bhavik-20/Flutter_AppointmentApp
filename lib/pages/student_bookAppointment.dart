d2db4785342b3be98afc0dac050311b3c76a8894
Bhavik-20 <bbhatt2005@gmail.com>
2021-04-14 12:14:30 +0530

    Book Appt Nofree checked

--- lib/pages/student_bookAppointment.dart
+++ lib/pages/student_bookAppointment.dart
@@ -149,7 +149,7 @@ class _student_bookAppointmentState extends State<student_bookAppointment> {
                           });
                         }
                         else {
-                          if (time == "" || time == "No Slots Available") {
+                          if (time == "") {
                             Fluttertoast.showToast(
                               backgroundColor: Colors.red,
                               msg: 'Please Select Time',
@@ -158,6 +158,15 @@ class _student_bookAppointmentState extends State<student_bookAppointment> {
                             );
                             print("Select time");
                           }
+                          if(time == "No Slots Available")
+                            {
+                              Fluttertoast.showToast(
+                                backgroundColor: Colors.red,
+                                msg: 'No Slots Available Currently',
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.BOTTOM,
+                              );
+                            }
                           if (full == null) {
                             Fluttertoast.showToast(
                               backgroundColor: Colors.red,
@@ -176,7 +185,7 @@ class _student_bookAppointmentState extends State<student_bookAppointment> {
                             );
                             print("Please enter purpose");
                           }
-                          if (time != "" && full != null &&
+                          if (time != "" && time != "No Slots Available" && full != null &&
                               purpose != null && this._currentStep == 4) {
                             Fluttertoast.showToast(
                               backgroundColor: Colors.green,
@@ -187,7 +196,7 @@ class _student_bookAppointmentState extends State<student_bookAppointment> {
                             print(purpose);
                             print("Completed");
                             setState(() =>loading=true);
-                            await Future.delayed(const Duration(milliseconds: 3000));
+                            await Future.delayed(const Duration(milliseconds: 2000));
                             await DatabaseService(uid: widget.teacher.teacher_id).removeSlot(time,remove_from);
                             await sendRequest(
                                 data.name,
@@ -207,7 +216,7 @@ class _student_bookAppointmentState extends State<student_bookAppointment> {
                                 widget.teacher.teacher_id,
                                 widget.teacher.url,
                                 data.url,);
-                            // loading = true;
+                            print("TIME: "+time);
                             await EmailSender().sendMailToTeacher(widget.teacher.email, data.name, data.email, data.year, data.branch, data.roll, purpose, full, time);
                             Navigator.pop(context);
                             Navigator.of(context).pushNamed('/st_dash');
